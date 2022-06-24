4f087bacfdf55e6ebf8dc1cc2f0ed52693313f98
BrunoVazquez <86581876+BrunoDucatiVazquez@users.noreply.github.com>
2022-06-24 15:24:41 -0300

    Update meu_site.py

--- meu_site.py
+++ meu_site.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-#criar a 1º pagina do site
+#configurando a base de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+#schame for user 
+class User(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Nome = db.Column(db.String(80), unique=True, nullable=False)
+    Sobrenome = db.Column(db.String(120), unique=True, nullable=False)
+    Email = db.Column(db.String(200), unique=True, nullable=False)
+  
+    def __repr__(self):
+        return '<User %r>' % self.Nome
 #route
-@app.route("/")
-#função
+@app.route("/home")
 def homepage():
     return render_template("homepage.html")
-
-@app.route("/contatos")
-def contatos():
-    return "Nossos contatos são:"
 
 #colocar o site no ar
 if __name__ == '__main__':
